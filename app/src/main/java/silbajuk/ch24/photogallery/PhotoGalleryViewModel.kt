fb59dd9d2916b09d127a88f37b01f79f70ecb495
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-10-11 20:59:47 +0900

    CH26 SearchView와 공유 프리퍼런스
    SearchView에서 사용자에게 응답하기
    (최근 쿼리 문자열 저장 및 검색 결과 변경하기)

--- app/src/main/java/silbajuk/ch24/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/silbajuk/ch24/photogallery/PhotoGalleryViewModel.kt
@@ -1,14 +1,29 @@
 package silbajuk.ch24.photogallery
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import retrofit2.http.Query
 
 class PhotoGalleryViewModel : ViewModel() {
 
     val galleryItemLiveData : LiveData<List<GalleryItem>>
 
+    private val fLickrFetchr = FLickrFetchr()
+    private val mutableSearchTerm = MutableLiveData<String>()
+
     init{
-        galleryItemLiveData = FLickrFetchr().searchPhotos("sky")
+        mutableSearchTerm.value = "sky"
+
+        galleryItemLiveData =
+            Transformations.switchMap(mutableSearchTerm){searchTerm ->
+                fLickrFetchr.searchPhotos(searchTerm)
+            }
+    }
+
+    fun fetchPhotos(query: String = ""){
+        mutableSearchTerm.value = query
     }
 
 }
