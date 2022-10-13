5923da46b4800f6b89edde2b905bfd0d64febb46
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-10-13 19:53:26 +0900

    CH26 SearchView와 공유 프리퍼런스
    공유 프리퍼런스를 이용한 간단한 데이터 보존
    (공유 프리퍼런스 기능을 제공하는 코틀린 object 추가하기)

---
+++ app/src/main/java/silbajuk/ch24/photogallery/QueryPreferences.kt
@@ -0,0 +1,21 @@
+package silbajuk.ch24.photogallery
+
+import android.content.Context
+import android.preference.PreferenceManager
+
+private const val PREF_SEARCH_QUERY = "searchQuery"
+
+class QueryPreferences {
+
+    fun getStoredQuery(context:Context) : String{
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        return prefs.getString(PREF_SEARCH_QUERY, "")!!
+    }
+
+    fun setStoredQuery(context: Context, query: String){
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .edit()
+            .putString(PREF_SEARCH_QUERY, query)
+            .apply()
+    }
+}
