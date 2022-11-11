bbe59847a294d1b946815b063f26262481518fee
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-11-12 08:07:35 +0900

    CH27 WorkManager
    사용자에게 알림 보여주기
    (newIntent(...)함수 추가하기)
    (알림 추가하기)

--- app/src/main/java/silbajuk/ch24/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/silbajuk/ch24/photogallery/PhotoGalleryActivity.kt
@@ -1,5 +1,7 @@
 package silbajuk.ch24.photogallery
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -14,6 +16,15 @@ class PhotoGalleryActivity : AppCompatActivity() {
                 .beginTransaction()
                 .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance())
                 .commit()
+        }
+    }
+
+    companion object{
+        fun newIntent(context: Context): Intent {
+            //PhotoGalleryActivity를 시작시키는 Intent인스턴스 반환
+            return Intent(context, PhotoGalleryActivity::class.java)
+            //PollWorker가 PhotoGalleryActivity.Intent(...)를 호출하고
+            //결과로 반환된 인텐트를 Pending Intent에 포함한 후 알람 설정
         }
     }
 }
