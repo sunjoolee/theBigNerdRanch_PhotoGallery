81ce0c37ef2cb4d44b18ad9b446b7fc4d3d3a1f4
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-12-17 11:09:25 +0900

    CH29 웹과 WebView의 브라우징
    WebChromeClient 사용하기
    (WebChromeClient 구현하기)

--- app/src/main/java/silbajuk/ch24/photogallery/PhotoPageFragment.kt
+++ app/src/main/java/silbajuk/ch24/photogallery/PhotoPageFragment.kt
@@ -6,14 +6,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 
 private const val ARG_URI = "photo_page_url"
 
 class PhotoPageFragment : VisibleFragment(){
     private lateinit var uri : Uri
     private lateinit var webView : WebView
+    private lateinit var progressBar : ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +31,26 @@ class PhotoPageFragment : VisibleFragment(){
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_photo_page, container, false)
+
+        progressBar = view.findViewById(R.id.progress_bar)
+        progressBar.max = 100
+
         webView = view.findViewById(R.id.web_view)
         webView.settings.javaScriptEnabled = true
+        webView.webChromeClient = object : WebChromeClient(){
+            override fun onProgressChanged(webView: WebView?, newProgress: Int) {
+                if(newProgress == 100){
+                    progressBar.visibility = View.GONE
+                }else {
+                    progressBar.visibility = View.VISIBLE
+                    progressBar.progress = newProgress
+                }
+            }
+
+            override fun onReceivedTitle(view: WebView?, title: String){
+                (activity as AppCompatActivity).supportActionBar?.subtitle = title
+            }
+        }
         webView.webViewClient = WebViewClient()
         webView.loadUrl(uri.toString())
 
